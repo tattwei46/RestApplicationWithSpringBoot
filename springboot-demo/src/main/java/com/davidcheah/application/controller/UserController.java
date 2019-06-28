e710e15f65f641f83cb4958003f513ca5a037c85
DavidCheah <tattwei46@gmail.com>
2019-06-28 23:22:33 +0800

    Added DELETE request to remove user

--- springboot-demo/src/main/java/com/davidcheah/application/controller/UserController.java
+++ springboot-demo/src/main/java/com/davidcheah/application/controller/UserController.java
@@ -3,6 +3,7 @@ package com.davidcheah.application.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,14 +25,20 @@ public class UserController {
 		return users;
 	}
 	
-	@GetMapping("/user/{id}")
+	@GetMapping("/users/{id}")
 	public User getUser(@PathVariable int id) {
 		return service.getUser(id);
 	}
-	
+		
 	@PostMapping("/users")
 	public User createUser(@RequestBody User user) {
 		User newUser = service.add(user);
 		return newUser;
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public User deleteUser(@PathVariable int id) {
+		User removedUser = service.delete(id);
+		return removedUser;
 	}
 }
