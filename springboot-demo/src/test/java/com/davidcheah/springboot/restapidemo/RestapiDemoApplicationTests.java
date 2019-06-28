1d94dd1cfa08b02546c722f22a47847bef229545
DavidCheah <tattwei46@gmail.com>
2019-06-28 22:01:47 +0800

    Initial version

---
+++ springboot-demo/src/test/java/com/davidcheah/springboot/restapidemo/RestapiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidcheah.springboot.restapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
