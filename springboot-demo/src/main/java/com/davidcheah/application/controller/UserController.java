73cf58e033db91e9dee96ace10a748e2f1694969
DavidCheah <tattwei46@gmail.com>
2019-06-28 22:57:57 +0800

    Added POST request to add new user

--- springboot-demo/src/main/java/com/davidcheah/application/controller/UserController.java
+++ springboot-demo/src/main/java/com/davidcheah/application/controller/UserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.davidcheah.application.services.UserService;
@@ -16,14 +18,20 @@ public class UserController {
 	@Autowired
 	private UserService service;
 
-	@GetMapping(path = "/users")
+	@GetMapping("/users")
 	public List<User> getAllUsers() {
 		List<User> users = service.getAllUsers();
 		return users;
 	}
 	
-	@GetMapping(path = "/user/{id}")
+	@GetMapping("/user/{id}")
 	public User getUser(@PathVariable int id) {
 		return service.getUser(id);
+	}
+	
+	@PostMapping("/users")
+	public User createUser(@RequestBody User user) {
+		User newUser = service.add(user);
+		return newUser;
 	}
 }
--- springboot-demo/src/main/java/com/davidcheah/application/services/UserService.java
+++ springboot-demo/src/main/java/com/davidcheah/application/services/UserService.java
@@ -12,10 +12,11 @@ public class UserService {
 
 	private static List<User> users = new ArrayList<User>();
 	
+	private static int usersCount = 0;
 	static {
-		users.add(new User(1, "Tony Stark", "ironman@avengers.inc"));
-		users.add(new User(1, "Steve Rogers", "captain@avengers.inc"));
-		users.add(new User(1, "Bruce Banner", "hulk@avengers.inc"));
+		users.add(new User(++usersCount, "Tony Stark", "ironman@avengers.inc"));
+		users.add(new User(++usersCount, "Steve Rogers", "captain@avengers.inc"));
+		users.add(new User(++usersCount, "Bruce Banner", "hulk@avengers.inc"));
 	}
 	
 	public List<User> getAllUsers() {
@@ -29,6 +30,12 @@ public class UserService {
 			}
 		}
 		return null;
+	}
+	
+	public User add(User user) {
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
 	}
 	
 }
